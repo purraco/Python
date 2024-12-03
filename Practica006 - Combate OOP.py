61c84a2e17a8e9fbc575d70793e7ab230495a570
usuario <usuario@git.com>
2024-12-03 13:31:09 +0100

    Utilizando atributos de la clase para llamar al Pokemon

--- Practica006 - Combate OOP.py
+++ Practica006 - Combate OOP.py
@@ -1,24 +1,20 @@
 """
-Si te parece bien, me gustaría colaborar en tu proyecto para poder aprender mutuamente tanto a colaborar
-como a utilizar github correctamente, ya que necesito practicar.
-Voy a tratar de sugerir algún cambio en el código.
-1- El primer cambio que te sugiero es utilizar Clases y Objetos para poder ir escalando poco a poco.
-2- El segundo cambio es utilizar los fstrings en vez del .format() para pasar variables a Strings
+ Importamos la funcion random para el ataque del Pokemon Oponente
+ Pikachu en este caso
 """
-
-# Importamos la funcion random para el ataque del Pokemon Oponente
-# Pikachu en este caso
 import random
-
-# Creamos la clase Ataque con los atributos nombre que guardará el nombre del ataque
-# y poder que guardará los puntos de daño de los ataques
+"""
+ Creamos la clase Ataque con los atributos nombre que guardará el nombre del ataque
+ y poder que guardará los puntos de daño de los ataques
+"""
 class Ataque:
     def __init__(self, nombre: str, poder: int) -> None:
         self.nombre = nombre
         self.poder = poder
-
-# Creamos la clase Pokemon con los atributos nombre, vida y ataques
-# y los métodos para agregar, elegir y recibir ataques
+"""
+ Creamos la clase Pokemon con los atributos nombre, vida y ataques
+ y los métodos para agregar, elegir y recibir ataques
+"""
 class Pokemon:
     def __init__(self, nombre: str, vida: int) -> None:
         self.nombre = nombre
@@ -35,9 +31,10 @@ class Pokemon:
         self.vida -= ataque.poder
         if self.vida < 0:
             self.vida = 0
-
-# Creamos la clase Combate que contendrá el Pokemon del jugador y del oponente
-# y los turnos de ataque de jugador y oponente. Empezará atacando el oponente.
+"""
+ Creamos la clase Combate que contendrá el Pokemon del jugador y del oponente
+ y los turnos de ataque de jugador y oponente. Empezará atacando el oponente.
+"""
 class Combate:
     def __init__(self, jugador: Pokemon, oponente: Pokemon) -> None:
         self.jugador = jugador
@@ -69,7 +66,7 @@ class Combate:
 
 # Explicación del juego, componentes y dinámica
     def jugar(self):
-        print("** COMBATE POKEMON -- Pikachu contra Squirtle **")
+        print(f"** COMBATE POKEMON -- {self.oponente.nombre} contra {self.jugador.nombre} **")
         print("Te reto a un combate entre personajes Pokemon. Yo controlo a Pikachu y tú a Squirtle.")
         print("Combatimos mientras ambos tengamos vida.\n")
         input(self.pulsa_para_continuar)
@@ -93,9 +90,9 @@ class Combate:
             print(f"A {self.oponente.nombre} le quedan {self.oponente.vida} puntos de vida.")
         
         if self.jugador.vida == 0:
-            print("\nLo siento, pero Pikachu te ha ganado.")
+            print(f"\nLo siento, pero {self.oponente.nombre} te ha ganado.")
         if self.oponente.vida == 0:
-            print("\n¡ENHORABUENA! Has derrotado a Pikachu.")
+            print(f"\n¡ENHORABUENA! Has derrotado a {self.oponente.nombre}.")
 
         print("\nEspero volver a verte pronto.")
 
@@ -108,17 +105,14 @@ squirtle = Pokemon("Squirtle", 90)
 squirtle.agregar_ataque("Placaje", 10)
 squirtle.agregar_ataque("Pistola de agua", 12)
 squirtle.agregar_ataque("Burbuja", 9)
-
-# Creamos el Combate:
-# El primer Pokemon que pasamos como argumento es el del jugador y el segundo el del oponente
+"""
+ Creamos el Combate:
+ El primer Pokemon que pasamos como argumento es el del jugador y el segundo el del oponente
+"""
 combate = Combate(squirtle, pikachu)
 combate.jugar()
 
 """
-Como primera aportación creo que es suficiente para ver si te parece bien continuar por esta línea de colaboración.
-
-Como siguientes pasos sugiero:
-1- Cambiar los sitios donde aparecen Pikachu y Squirtle escritos "a mano" por una llamada al Pokemon.nombre que corresponda
-    por ejemplo en las líneas 72, 73, 96, 98
+Siguiente Commit Previsto:
 2- Cambiar los valores escritos "a mano" en la función turno_atacar_jugador() por una llamada a los mismos
 """
